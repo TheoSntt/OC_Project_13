f4c1cf103b543e7670cd59e928e2bbb963a31e93
Théo <sinatti.theo@gmail.com>
2023-09-13 18:33:50 +0200

    Refactor the profiles urls, views, and templates : URLs, views, and templates have been moved from the main app to the profiles app.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', include('lettings.urls')),
+    path('lettings', include('lettings.urls')),
     # path('lettings/', views.lettings_index, name='lettings_index'),
     # path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    # path('profiles/', include('profiles.urls')),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles', include('profiles.urls')),
+    # path('profiles/', views.profiles_index, name='profiles_index'),
+    # path('profiles/<str:username>/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
