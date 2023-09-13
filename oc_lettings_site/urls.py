2a9c0c486552a7b0822ecfcd3d19385b66aad418
Théo <sinatti.theo@gmail.com>
2023-09-13 18:28:00 +0200

    Refactor the lettings urls, views, and templates : URLs, views, and templates have been moved from the main app to the lettings app.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
+    path('lettings/', include('lettings.urls')),
+    # path('lettings/', views.lettings_index, name='lettings_index'),
+    # path('lettings/<int:letting_id>/', views.letting, name='letting'),
+    # path('profiles/', include('profiles.urls')),
     path('profiles/', views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
