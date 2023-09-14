01c091409a137d5f1bc663e635e041d2330e5a61
Théo <sinatti.theo@gmail.com>
2023-09-14 10:26:33 +0200

    Add necessary templates, views and settings to handle 404 and 500 errors

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Returns the home page of the app
 def index(request):
     return render(request, 'index.html')
+
+
+# Returns the error 404 page for the app
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+# Returns the error 500 page for the app
+def handler500(request):
+    return render(request, '500.html', status=500)
